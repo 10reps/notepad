3c194c772934a4a79320767ff81fe41f2076188b
Seohong Kim <greyxcx@gmail.com>
2022-11-01 23:03:42 +0900

    Add event

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-// 텍스트 작성 후 ADD버튼 누르면 추가
-
-const add = document.querySelector('.add-button');
+const items = document.querySelector('.items');
 const write = document.querySelector('.write');
+const addBtn = document.querySelector('.add__button');
 
-add.addEventListener('click', event => {
-    const move = document.createElement('li');
-    const ddd = document.createTextNode('sss');
-    move.appendChild(ddd);
-    document.body.appendChild('li');
+function onAdd() {
+    const text = write.value;
+
+    const item = createItem(text);
+
+    items.appendChild(item);
+
+}
+
+
+function createItem(text) {
+
+    const itemRow = document.createElement('li');
+    itemRow.setAttribute('class','item__row');
+
+    const name = document.createElementNS('div');
+    name.setAttribute('class', 'item__name');
+
+    const deleteBtn = document.createElementNS('button');
+    deleteBtn.setAttribute('class', 'trashBtn');
+
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    
+    deleteBtn.addEventListener('click', () => {
+        items.removeChild(itemRow);
+    });
+
+    itemRow.appendChild(name);
+    itemRow.appendChild(deleteBtn);
+
+    return itemRow;
+}
+
+
+addBtn.addEventListener('click', event => {
+    console.log('dfdf');
+    onAdd();
 });
-
-// 버튼 누르면 삭제
